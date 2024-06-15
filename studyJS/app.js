2cfd0b536513f4541aca572e3d5e6385045e28ec
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-16 01:37:46 +0900

    clear

--- studyJS/app.js
+++
@@ -1,9 +0,0 @@
-const h1 =  document.querySelector(".hello h1");
-const h1s = document.querySelectorAll(".hello h1")
-
-handleTitleClick = () => {
-    h1.classList.toggle("clicked")
-    console.log(h1.classList)
-}
-
-h1.addEventListener("click", handleTitleClick);
