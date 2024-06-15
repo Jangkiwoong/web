95d7c98bb222352391a8627aee089a74c729ee5b
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-15 22:10:44 +0900

    addEventListenr

--- studyJS/app.js
+++ studyJS/app.js
@@ -1,5 +1,8 @@
 const title =  document.querySelector(".hello h1");
 const titles = document.querySelectorAll(".hello h1")
 
-console.log(title);
-console.log(titles);
+handleTitleClick = () => {
+    console.log("tite was clicked!");
+}
+
+title.addEventListener("click", handleTitleClick);
