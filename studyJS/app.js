9a103f943b230349c753c931800341f3c3ce7eea
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-15 23:45:08 +0900

    eventer

--- studyJS/app.js
+++ studyJS/app.js
@@ -1,8 +1,28 @@
-const title =  document.querySelector(".hello h1");
-const titles = document.querySelectorAll(".hello h1")
+const h1 =  document.querySelector(".hello h1");
+const h1s = document.querySelectorAll(".hello h1")
 
 handleTitleClick = () => {
-    console.log("tite was clicked!");
+    h1.innerText = "tite was clicked!";
 }
 
-title.addEventListener("click", handleTitleClick);
+handleMouseEnter = () => {
+    h1.innerText = "Mouse is here!"
+}
+
+handleMouseLeave = () => {
+    h1.innerText = "Mouse is gone";
+}
+
+handleWindowResize = () => {
+    document.body.style.backgroundColor = "tomato";
+}
+
+handleWindowCopy = () => {
+    alert("copier!")
+}
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
