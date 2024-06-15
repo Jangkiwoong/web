01156f77058fd6ed5823232614ef90f917d46bb1
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-16 00:31:19 +0900

    classList

--- studyJS/app.js
+++ studyJS/app.js
@@ -2,10 +2,13 @@ const h1 =  document.querySelector(".hello h1");
 const h1s = document.querySelectorAll(".hello h1")
 
 handleTitleClick = () => {
-    const currentColor = h1.style.color;
-    h1.innerText = "tite was clicked!";
-    if (currentColor === "blue") {h1.style.color = "tomato";}
-    else {h1.style.color = "blue";}
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
+    } else {
+        h1.classList.add(clickedClass);
+    }
+    console.log(h1.classList)
 }
 
 h1.addEventListener("click", handleTitleClick);
