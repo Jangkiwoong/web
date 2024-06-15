6fa96d3a4ee212083626fcc7e07cafc07cc6eb45
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-15 23:56:39 +0900

    style in js

--- studyJS/app.js
+++ studyJS/app.js
@@ -2,27 +2,10 @@ const h1 =  document.querySelector(".hello h1");
 const h1s = document.querySelectorAll(".hello h1")
 
 handleTitleClick = () => {
+    const currentColor = h1.style.color;
     h1.innerText = "tite was clicked!";
+    if (currentColor === "blue") {h1.style.color = "tomato";}
+    else {h1.style.color = "blue";}
 }
 
-handleMouseEnter = () => {
-    h1.innerText = "Mouse is here!"
-}
-
-handleMouseLeave = () => {
-    h1.innerText = "Mouse is gone";
-}
-
-handleWindowResize = () => {
-    document.body.style.backgroundColor = "tomato";
-}
-
-handleWindowCopy = () => {
-    alert("copier!")
-}
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
+h1.addEventListener("click", handleTitleClick);
