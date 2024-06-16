bc9d325d2bbc2d48e17ec916014aba09e3671b16
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-17 00:12:47 +0900

    clock

--- studyJS/app.js
+++ studyJS/app.js
@@ -3,15 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const onLoginSubmit = (event) => {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetinhs();
+    console.log(loginInput.value);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreetinhs() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null ) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetinhs();
+}
