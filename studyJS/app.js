a626d2f5f028f50fba4933c0b454c9b09117c163
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-16 20:53:42 +0900

    set localStorage

--- studyJS/app.js
+++ studyJS/app.js
@@ -8,6 +8,7 @@ const onLoginSubmit = (event) => {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username}`
     greeting.classList.remove(HIDDEN_CLASSNAME)
     console.log(username);
