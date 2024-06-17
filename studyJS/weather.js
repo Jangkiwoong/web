8a51ef9785999288f36dfbd17edd044e66fec246
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-17 19:54:08 +0900

    end

--- studyJS/weather.js
+++ studyJS/weather.js
@@ -1,9 +1,22 @@
+const cityApi = document.querySelector("#weather span:first-child");
+const weatherApi = document.querySelector("#weather span:last-child")
+
+const API_KEY = ""
+const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`
+
 onGeoOk = (position) => {
     const x = position.coords.longitude;
     const y = position.coords.latitude;
     console.log("You live in x = ", x, " y = ",y);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${y}&lon=${x}&appid=${API_KEY}&units=metric`
+    fetch(url).then(response => response.json().then(data => {
+        const name = data.name
+        const weather = `${data.weather[0].main} / ${data.main.temp}`;
+        cityApi.innerText = name;
+        weatherApi.innerText = weather;
+    }));
 }
-
+ 
 onGeoError = () => {
     alert("Can't find you. No weather for you.");
 }
