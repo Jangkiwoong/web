2a050ae962d81a09db4f08f09b1461a9be4c0ff3
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-17 18:19:01 +0900

    todo.js

--- studyJS/todo.js
+++ studyJS/todo.js
@@ -4,21 +4,24 @@ const todoList = document.querySelector("#todo-list")
 
 const TODOS_KEY = "todos";
 
-const todos = [];
+let todos = [];
 
 saveTodo = () => {
     localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 deleteTodo = (event) => {
-    const li = event.target.parentElement
+    const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter((todo) => todo.id !== parseInt(li.id));
+    saveTodo();
 }
 
 const paintTodo = (newTodo) => {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
@@ -31,11 +34,20 @@ const handleTodoSubmit = (event) => {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    todos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    };
+    todos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodo(); 
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 
-const savedTodos = localStorage.getItem(TODOS_KEY)
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos !== null) {
+    const parsedTodos = JSON.parse(savedTodos);
+    todos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
