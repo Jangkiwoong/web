84bb72c2ac41586a0c81497763632b9122be43ea
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-17 19:02:12 +0900

    get x, y

--- studyJS/weather.js
+++ studyJS/weather.js
@@ -1,9 +1,11 @@
 onGeoOk = (position) => {
-    
+    const x = position.coords.longitude;
+    const y = position.coords.latitude;
+    console.log("You live in x = ", x, " y = ",y);
 }
 
 onGeoError = () => {
-    alert("Can't find you. No weather for you.")
+    alert("Can't find you. No weather for you.");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); 
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
