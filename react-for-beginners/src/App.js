8e08db0a84984acd4c3721f4b1c752edbacb5fc6
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-22 17:27:28 +0900

    css

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,15 @@
+import Button from "./component/Button.js";
+
 function App() {
   return (
     <div>
       <div>
         <h1>Welcome back to React!</h1>
+        <Button text={"Continue"} />
       </div>
     </div>
   );
 }
 
 export default App;
+ 
---
+++ react-for-beginners/src/component/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types"
+
+const Button = ({text}) => {
+    return (
+        <button>{text}</button>
+    );
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+}
+
+export default Button;
