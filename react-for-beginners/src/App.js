53c586f13cd4db5ad9a16e817fe7a691f9d0f8cc
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-22 22:56:02 +0900

    useEffact

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -9,10 +9,12 @@ import styles from "./App.module.css"
 function App() {
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState("?");
-  console.log("I run all the time.");
+  useEffect(() => {console.log("I run all the time.")});
   useEffect(() => {console.log("I run only once.")}, []);
   useEffect(() => { if (keyword !== "?" && keyword !== "") {console.log("I run when 'keyword' changes.", keyword)}}, [keyword]);
   useEffect(() => { if (counter !== 0 && counter !== "") {console.log("I run when 'counter' changes.", counter)}}, [counter]);
+  useEffect(() => { if (counter !== 0 && counter !== "") {console.log("I run when 'counter' changes.", counter)}}, [counter]);
+  useEffect(() => { console.log("I run when 'keyword & counter' changes.", counter)}, [counter, keyword]);
   // const MemorizedSearch = React.memo(Search);
   return (
     <div>
