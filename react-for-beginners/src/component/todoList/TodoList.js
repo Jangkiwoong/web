ccefea31c0f88b02a283f71eabddcd635e52ac8e
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-23 17:04:26 +0900

    [Feat] Todays list

--- react-for-beginners/src/component/todoList/TodoList.js
+++ react-for-beginners/src/component/todoList/TodoList.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 const TodoList = () => {
     const [toDo, setTodo] = useState("");
-    console.log("I'm working");
+    const [toDos, setTodos] = useState([]); 
+    console.log(toDos);
     return (
         <div>
-            <form onSubmit={ (event) => { event.preventDefault(); if (toDo === "") {return;} else {console.log(toDo)}; setTodo("") } }>
+            <h1>My To days List /{toDos.length}</h1>
+            <form onSubmit={ (event) => { event.preventDefault(); if (toDo === "") {return;}; setTodo(""); setTodos((prov) => [toDo, ...toDos])} }>
                 <input value={toDo} onChange={ (event) => setTodo(event.target.value)} type="text" placeholder="Write your to do..." />
                 <button>Add To Do</button>
             </form>
