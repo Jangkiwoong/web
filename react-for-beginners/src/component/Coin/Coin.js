db3c313eb391a1a0e8e1268a6c5f3a78cfbe4fdd
jangkiwoong <wkdrldnd99@gmail.com>
2024-06-23 20:04:15 +0900

    coins btc

--- react-for-beginners/src/component/Coin/Coin.js
+++ react-for-beginners/src/component/Coin/Coin.js
@@ -13,13 +13,13 @@ const Coin = () => {
   }, []);
   return (
     <div>
-      <h1>The Coins!</h1>
+      <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
       <ul>
         {coins.map((item) => { 
             return (
                 <li key={item.id}>
-                    {item.name} ({item.symbol}) : ${item.quotes.USD.price}USD
+                    {item.name} ({item.symbol}) : ${item.quotes.USD.price}USD / #{item.quotes.USD.price * 0.00001556}BTC
                 </li>
             );
         })}
